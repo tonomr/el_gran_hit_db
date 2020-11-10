7d0469eb83e61f8be418252a52e2668b5bcb206e
Antonio Magaña <59188525+tonomr@users.noreply.github.com>
2020-11-10 00:30:50 -0600

    Update videojuego_dao.py

--- videojuego_dao.py
+++ videojuego_dao.py
@@ -1,7 +1,7 @@
 # CLASE VIDEOJUEGO DAO (DATA ACCESS OBJECT) PARA ADMINISTRAR LOS DATOS DE LA BASE DE DATOS
 
 from videojuego import Videojuego # IMPORTAMOS LA CLASE VIDEOJUEGO
-from conexion import Conexion # IMPORTAMOS LA CONEXION
+from cursor import CursorDelPool # IMPORTAMOS LA CONEXION
 from logger_conf import logger # IMPORTAMOS EL LOGGER
 
 # LA CLASE VIDEOJUEGODAO CONTIENE LAS CONSTANTES PARA HACER LAS CONSULTAS A LA BASE DE DATOS Y SUS RESPECTIVOS METODOS PARA MOSTRARLAS AL USUARIO
@@ -13,39 +13,29 @@ class VideojuegoDao:
     # METODO SELECT PARA MOSTRAR LOS REGISTROS DE LA TABLA VIDEOJUEGOS
     @classmethod
     def seleccionar(cls):
-        cursor = Conexion.obtenerCursor()
-        logger.debug(cursor.mogrify(cls.__SELECT)) # SENTENCIA A EJECUTAR
-        cursor.execute(cls.__SELECT) # EJECUCION DE LA SENTENCIA
-        registros = cursor.fetchall() # GUARDAMOS LOS REGISTROS
+        with CursorDelPool() as cursor:
+            logger.debug(cursor.mogrify(cls.__SELECT)) # SENTENCIA A EJECUTAR
+            cursor.execute(cls.__SELECT) # EJECUCION DE LA SENTENCIA
+            registros = cursor.fetchall() # GUARDAMOS LOS REGISTROS
         
-        videojuegos = [] # DEFINICION DE UNA LISTA PARA IMPRIMIR LOS RESULTADOS
-        # POR CADA REGISTRO GUARDADO EN REGISTROS, AGREGARLO A LA LISTA videojuegos COMO OBJETOS
-        for registro in registros:
-            videojuego = Videojuego(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5], registro[6], registro[7], registro[8], registro[9])
-            videojuegos.append(videojuego)
+            videojuegos = [] # DEFINICION DE UNA LISTA PARA IMPRIMIR LOS RESULTADOS
+            # POR CADA REGISTRO GUARDADO EN REGISTROS, AGREGARLO A LA LISTA videojuegos COMO OBJETOS
+            for registro in registros:
+                videojuego = Videojuego(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5], registro[6], registro[7], registro[8], registro[9])
+                videojuegos.append(videojuego)
 
-        Conexion.cerrarConexiones() # CERRAMOS LA CONEXIONES
-        return videojuegos # RETORNAMOS LA LISTA
+            return videojuegos # RETORNAMOS LA LISTA
     
     # METODO INSERTAR PARA AGREGAR REGISTROS
     @classmethod
     def insertar(cls, videojuego):
-        try:
-            conexion = Conexion.obtenerConexion() # OBTENEMOS EL OBJETO CONEXION PARA PODER HACER UN commit O rollback
-            cursor = Conexion.obtenerCursor() # OBTENEMOS EL OBJETO CURSOR PARA EJECUTAR SENTENCIAS
+        with CursorDelPool() as cursor:
             logger.debug(cursor.mogrify(cls.__INSERTAR)) # SENTENCIA A EJECUTAR
             logger.debug(f"Videojuego a insertar: {videojuego}") # SE IMPRIME EL OBJETO videojuego A INSERTAR
             valores = (videojuego.getCodigo(), videojuego.getNombre(), videojuego.getCategoria(), videojuego.getPrecio(), videojuego.getDescripcion(), videojuego.getClasificacion(), videojuego.getCopias(), videojuego.getPublicacion(), videojuego.getEstado(), videojuego.getDesarrolladora())
             cursor.execute(cls.__INSERTAR, valores) # EJECUCION DE LA SENTENCIA
-            conexion.commit() # SI NO HUBO ERRORES, GUARDA LOS CAMBIOS
-            return cursor.rowcount # RETORNAMOS EL NUMERO DE INSERCIONES
-        
-        except Exception as excepcion:
-            conexion.rollback() # SI HUBO ERRORES, NO GUARDAR LOS CAMBIOS
-            logger.error(f"ERROR, Insertando videojuego: {excepcion}")
             
-        finally:
-            Conexion.cerrarConexiones() # CERRAMOS LAS CONEXIONES SI FALLA O NO FALLA
+            return cursor.rowcount # RETORNAMOS EL NUMERO DE INSERCIONES
 
 # PRUEBA DE CONFIGURACION (SOLO SE EJECUTARA CUANDO SE EJECUTE ESTE MODULO)
 if __name__ == "__main__":
