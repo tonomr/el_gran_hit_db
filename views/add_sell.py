60f1b21d21c1b3d494da1ee813c806850176044d
PacoCHAMORRRO <francisco.chamorro2924@alumnos.udg.mx>
2020-11-29 22:43:41 -0600

    Add design to AddGame

--- views/add_sell.py
+++ views/add_sell.py
@@ -15,11 +15,17 @@ class AddSell(ttk.Frame):
         self.root = parent # Set parent
         self.init_gui() # Iniciar la interfaz gráfica
 
-    # Limpia la ventana, recorre todos los widgets eliminandolos uno por uno
-    # Nota: No elimina la configuración de columnas.
-    def clear_frames(self):
-        for widget in self.root.winfo_children():
-            widget.destroy()
+    # Reset form
+    def reset_form(self):
+        self.game_id.delete(0, END)
+        self.sell_date.delete(0, END)
+        self.sell_quantity.delete(0, END)
+        self.sell_address.delete(0, END)
+        self.customer_id.delete(0, END)
+
+    # Go back to main menu
+    def cancel_to_main(self):
+        self.root.destroy()
 
     # Add video game, function called from game
     def add_sell(self):
@@ -29,65 +35,72 @@ class AddSell(ttk.Frame):
         #subtotal = self.sell_subtotal.get()
         #total = self.sell_total.get()
         #delivery = self.sell_delivery.get()
-        id_game = self.sell_idgame.get()
-        id_customer = self.sell_idcustomer.get()
+        id_game = self.game_id.get()
+        id_customer = self.customer_id.get()
         # Create Purchase instance
         #sell = Venta(fecha_venta=date, cantidad_venta=quantity, subtotal_venta=subtotal, total_venta=total, direccion_envio=delivery, codigo_videojuego=id_game, codigo_cliente=id_customer)
         sell = Venta(fecha_venta=date, cantidad_venta=quantity, codigo_videojuego=id_game, codigo_cliente=id_customer)
         VentaDao.insertar(sell)
 
-    # Go back to previous window
-    def back_to_prev(self):
-        #self.clear_frames()
-        #GameWindow(self.root)
-        pass
+        confirm = messagebox.askyesno(parent=self.root, message='Agregado correctamente, ¿Desea agregar otro?', 
+                            icon='question', title='Agregado extiosamente')
 
-    # Go back to main menu
-    def cancel_to_main(self):
-        #self.clear_frames()
-        pass
+        if (confirm == True):
+            self.reset_form()
+        else:
+            self.cancel_to_main()
+
 
     # Init the Graphic User Interface
     def init_gui(self):
-        self.clear_frames()
         # WINDOW TITLE
         self.root.title("Agregar Venta")
 
         # WINDOW SIZE
         self.root.geometry("600x400")
-        # Configuración de columnas
-        #self.grid_columnconfigure(0, weight=1)
-        #self.grid_columnconfigure(2, weight=1)
+        
+        ############################## FRAMES ###############################
+        # -------------------------- HEADER FRAME ---------------------------
+        # Widgets
+        self.header = PhotoImage(file='add-sell.png')
+        self.header_frame = ttk.Frame(self.root)          # Crea un frame
+        self.header_label = ttk.Label(self.header_frame)  # Crea una etiqueta para la img
+        self.header_label['image'] = self.header          # Setea el label al tipo imagen
+        # Grid
+        self.header_label.grid(row=0, column=0)
+        
+        
+        # ------------------------- INPUTS FRAME --------------------------------
+        self.inputs_frame = ttk.Frame(self.root)
+        # Widgets
+        self.label_gameid = ttk.Label(self.inputs_frame, text="ID de videojuego")
+        self.game_id = ttk.Entry(self.inputs_frame, width=60)
+        
+        self.label_date = ttk.Label(self.inputs_frame, text="Fecha de venta")
+        self.sell_date = ttk.Entry(self.inputs_frame, width=15)
 
-        #--------------------------------------------------------------------------
-        # LABEL AND INPUT FIELDS    
-        # Label es una etiqueta, que describe lo que deberías escribir en el campo
-        # Entry es un widget del tipo input
-        #--------------------------------------------------------------------------
+        self.label_quantity = ttk.Label(self.inputs_frame, text="Cantidad")
+        self.sell_quantity = ttk.Entry(self.inputs_frame, width=15)
 
-        # Fecha de venta
-        self.label_date = ttk.Label(self.root, text="Fecha de venta")
-        self.sell_date = ttk.Entry(self.root, width=50)
-        # Cantidad
-        self.label_quantity = ttk.Label(self.root, text="Cantidad")
-        self.sell_quantity = ttk.Entry(self.root, width=50)
-        '''
-        # Subtotal
-        self.label_subtotal = ttk.Label(self.root, text="Subtotal")
-        self.sell_subtotal = ttk.Entry(self.root, width=50)
-        # Total  
-        self.label_total = ttk.Label(self.root, text="Total")
-        self.sell_total = ttk.Entry(self.root, width=15)
-        # Dirección de envío  
-        self.label_delivery = ttk.Label(self.root, text="Envío a")
-        self.sell_delivery = ttk.Entry(self.root, width=15)
-        '''
-        # Id del videjuego
-        self.label_idgame = ttk.Label(self.root, text="ID videojuego")
-        self.sell_idgame = ttk.Entry(self.root, width=50)
-        # Id del empleado
-        self.label_idcustomer = ttk.Label(self.root, text="ID cliente")
-        self.sell_idcustomer = ttk.Entry(self.root, width=50)
+        self.label_address = ttk.Label(self.inputs_frame, text="Dirección envío")
+        self.sell_address = ttk.Entry(self.inputs_frame, width=15)
+
+        self.label_customer = ttk.Label(self.inputs_frame, text="ID de cliente")
+        self.customer_id = ttk.Entry(self.inputs_frame, width=50)
+
+        # GRID inputs
+        self.label_gameid.grid(row=0, column=0, sticky=("w"), columnspan=2)
+        self.game_id.grid(row=0, column=2, sticky=("we"), columnspan=3)
+        self.label_date.grid(row=2, column=0, sticky=("w"))
+        self.sell_date.grid(row=2, column=2, sticky=("we"))
+        self.label_quantity.grid(row=3, column=0, sticky=("w"))
+        self.sell_quantity.grid(row=3, column=2, sticky=("we"))
+
+        self.label_address.grid(row=4, column=0, sticky=("w"))
+        self.sell_address.grid(row=4, column=2, sticky=("we"))
+
+        self.label_customer.grid(row=5, column=0, sticky=("w"), columnspan=2)
+        self.customer_id.grid(row=5, column=2, sticky=("we"), columnspan=3)
 
         #--------------------------------------------------------------------------
         # BUTTONS
@@ -95,45 +108,28 @@ class AddSell(ttk.Frame):
         # Al presionar este parametro, mandamos llamar la función que específicamos
         # en 'command'
         #--------------------------------------------------------------------------
+        # Buttons Frame
+        self.btns_frame = ttk.Frame(self.root)
         # Add
-        self.btn_add = ttk.Button(
-            self.root, text='Agregar', width=30, command=self.add_sell)
-        # Back
-        self.btn_back = ttk.Button(
-            self.root, text='Atrás', width=30, command=self.back_to_prev)
+        self.btn_update = ttk.Button(
+            self.btns_frame, text='Agregar', width=20, command=self.add_sell)
+        # Reset
+        self.btn_reset = ttk.Button(
+            self.btns_frame, text='Reset', width=20, command=self.reset_form)
         # Cancel
         self.btn_cancel = ttk.Button(
-            self.root, text='Cancelar', width=30, command=self.cancel_to_main)
-
-        # -------------------------------------------------------------------------
-        # GRID 
-        # Especificamos las columnas, las filas. El ancho de las columnas NO es estático
-        # Su ancho depende del widget que haya dentro de ella, es decir, si no
-        # existe nada en una columna dada, el espacio no se verá reflejado en el grid de
-        # de nuestra ventana.
-        # Sticky puede llevar cualquier combinacion de caracteres
-        # Para alinear o estierar el widget en alguna de las direcciones cardinales
-        # 'w' west (izquierda) 'e' east (derecha) 'n' north 's' south
-        #---------------------------------------------------------------------------
-        # Inputs
-        self.label_date.grid(row=1, column=0)
-        self.sell_date.grid(row=1, column=2, sticky=("we"))
-        self.label_quantity.grid(row=2, column=0)
-        self.sell_quantity.grid(row=2, column=2, sticky=("we"))
-        '''
-        self.label_subtotal.grid(row=3, column=0)
-        self.sell_subtotal.grid(row=3, column=2, sticky=("we"))
-        self.label_total.grid(row=4, column=0)
-        self.sell_total.grid(row=4, column=2, sticky=("we"))
-        self.label_delivery.grid(row=5, column=0)
-        self.sell_delivery.grid(row=5, column=2, sticky=("we"))
-        '''
-        self.label_idgame.grid(row=6, column=0)
-        self.sell_idgame.grid(row=6, column=2, sticky=("we"))
-        self.label_idcustomer.grid(row=7, column=0)
-        self.sell_idcustomer.grid(row=7, column=2, sticky=("we"))
+            self.btns_frame, text='Cancelar', width=20, command=self.cancel_to_main)
 
         # Buttons
-        self.btn_add.grid(row=19, column=2, sticky=("we"))
-        #self.btn_back.grid(row=19, column=3, sticky=("we"))
-        #self.btn_cancel.grid(row=19, column=4, sticky=("we"))
+        self.btn_update.grid(row=9, column=1, pady=15)
+        self.btn_reset.grid(row=9, column=2, pady=15)
+        self.btn_cancel.grid(row=9, column=3, pady=15)
+
+        # -------------------------- GRID ROOT -------------------------------
+        self.header_frame.grid(row=0, column=0)
+        self.inputs_frame.grid(row=4, column=0)
+        self.btns_frame.grid(row=5, column=0, padx=10, sticky=("e"))
+        
+        # Padding
+        for child in self.inputs_frame.winfo_children():
+            child.grid_configure(padx=8, pady=3)
